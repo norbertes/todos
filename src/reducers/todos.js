f8c015cfa7784b03849eb176e539b980872c713a
Norbert Sienkiewicz <norbert.sienkiewicz@gmail.com>
2016-11-21 21:08:17 +0100

    checkpoint after 12

--- src/reducers/todos.js
+++ src/reducers/todos.js
@@ -1,38 +1,54 @@
-const todo = (state, action) => {
+import { combineReducers } from 'redux';
+import todo from './todo';
+
+const byId = (state = {}, action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return {
-        id: action.id,
-        text: action.text,
-        completed: false,
-      };
     case 'TOGGLE_TODO':
-      if (state.id !== action.id) {
-        return state;
-      }
       return {
         ...state,
-        completed: !state.completed,
+        [action.id]: todo(state[action.id], action),
       };
     default:
       return state;
   }
 };
 
-const todos = (state = [], action) => {
+const allIds = (state = [], action) => {
   switch (action.type) {
     case 'ADD_TODO':
-      return [
-        ...state,
-        todo(undefined, action),
-      ];
-    case 'TOGGLE_TODO':
-      return state.map(t =>
-        todo(t, action)
-      );
+      return [...state, action.id];
     default:
       return state;
   }
 };
 
+const todos = combineReducers({
+  byId,
+  allIds,
+});
+
+// default export is always the reducer
 export default todos;
+
+const getAllTodos = (state) =>
+  state.allIds.map(id => state.byId[id]);
+
+
+// function that prepares data to be displayed by UI
+// (selector function - selectrs from current state)
+// (data is prepared in reducer, not component itself)
+export const getVisibleTodos = (state, filter) => {
+  const allTodos = getAllTodos(state);
+
+  switch (filter) {
+    case 'all':
+      return allTodos;
+    case 'completed':
+      return allTodos.filter(t => t.completed);
+    case 'active':
+      return allTodos.filter(t => !t.completed);
+    default:
+      throw new Error(`Unknown filter: ${filter}.`);
+  }
+};
